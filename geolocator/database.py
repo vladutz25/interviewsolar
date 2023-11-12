c9943d617495a4ca2f5c1a08b8ab8fe3f70a54d4
Vlad <vlad.strugaiu25@gmail.com>
2023-11-12 14:17:27 +0100

    added DB functionality for write and create_table

---
+++ geolocator/database.py
@@ -0,0 +1,39 @@
+import os
+import sqlite3
+from sqlite3 import Error
+
+class Database:
+    def __init__(self,):
+        self.connection = None
+
+    def connect(self, db_path):
+        try:
+            self.connection = sqlite3.connect(db_path)
+            self._create_table()
+        except Error as error:
+            print(error)
+        finally:
+            if self.connection:
+                self.connection.close()
+
+    def write(self, latitude, longitude, city, country):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute("INSERT INTO geolocation (latitude, longitude, city, country_code) VALUES (?, ?, ?, ?)",
+                           (latitude, longitude, city, country))
+            self.connection.commit()
+        except Error as error:
+            print(error)
+
+    def _create_table(self):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute('''CREATE TABLE IF NOT EXISTS geolocation (
+    latitude REAL NOT NULL,
+    longitude REAL NOT NULL,
+    city TEXT NOT NULL,
+    country_code TEXT NOT NULL
+)
+''')
+        except Error as error:
+            print(error)
